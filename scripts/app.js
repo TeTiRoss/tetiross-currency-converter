00bf5920dc79e64a21db485e351ca93872b933c3
TeTiRoss <rostyk.s@hmarynka.com>
2016-09-20 15:22:13 +0300

    coloring div react

--- scripts/app.js
+++ scripts/app.js
@@ -1,45 +1,23 @@
-var CounterDisplay = React.createClass({
+var green = "rgb(74, 195, 130)";
+var yellow = "rgb(222, 228, 105)";
+
+var ColoringDiv = React.createClass({
+  getInitialState: function () {
+    return {color: green}
+  },
+
+  changeColor: function() {
+    var newColor = this.state.color == green ? yellow : green;
+    this.setState({color: newColor})
+  },
+
   render: function() {
     return (
-      <div>
-        <div>{this.props.counterProp}</div>
-        <br />
-        <div className='btn-group'>
-          <button className='btn btn-info' onClick={this.props.incrementCounter}>+</button>
-          <button className='btn btn-danger' onClick={this.props.decrementCounter}>-</button>
-        </div>
+      <div style={{backgroundColor: this.state.color}} onClick={this.changeColor}>
       </div>
-    );
+    )
   }
 });
 
-var Counter = React.createClass({
-    getInitialState: function() {
-    return {
-        counter: 0
-    };
-  },
-  handleIncrement(){
-    // Update counter state
-    this.setState({counter : this.state.counter+1});
-  },
-  handleDecrement(){
-      // Update counter state
-    this.setState({counter : this.state.counter-1});
-  },
-  render: function() {
-    // Pass down handlers to CounterDisplay component
-    return <div>
-            <h2>{this.props.name}</h2>
-            <CounterDisplay
-            counterProp={this.state.counter}
-          incrementCounter={this.handleIncrement}
-          decrementCounter={this.handleDecrement}></CounterDisplay>
-      </div>;
-  }
-});
-
-ReactDOM.render(
-  <Counter name={'ReactJS counter'} />,
-  document.getElementById('container')
-);
+// Put container element id below
+// ReactDOM.render(<ColoringDiv />, document.getElementById(''))
