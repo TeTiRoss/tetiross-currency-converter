7f25abcc51deefca3b04feb5b1361ac0d2eec572
TeTiRoss <rostyk.s@hmarynka.com>
2016-09-22 16:21:08 +0300

    all react

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,49 @@
-var green = "rgb(74, 195, 130)";
-var yellow = "rgb(222, 228, 105)";
-
-var ColoringDiv = React.createClass({
-  getInitialState: function () {
-    return {color: green}
-  },
-
-  changeColor: function() {
-    var newColor = this.state.color == green ? yellow : green;
-    this.setState({color: newColor})
-  },
-
-  render: function() {
+var App = React.createClass({
+  render: function () {
     return (
-      <div style={{backgroundColor: this.state.color}} onClick={this.changeColor}>
+      <div>
+        <Header />
+        <Bio />
       </div>
-    )
+    );
   }
 });
 
-// Put container element id below
-// ReactDOM.render(<ColoringDiv />, document.getElementById(''))
+var Header = React.createClass({
+  render: function () {
+    return (
+      <div className='col-md-8 col-md-offset-2'>
+				<h1 id='header_txt'>TeTiRoss</h1>
+				<a id="git_logo" className="social_icons" target='blank' href="https://github.com/TeTiRoss"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+				<a id="facebook_logo" className="social_icons" target='blank' href="https://www.facebook.com/rostyk.semanyshyn"><i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+        <a id="linkedin_logo" className="social_icons" target='blank' href="https://www.linkedin.com/in/rostyk-semanyshyn-04528a116"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+        <hr id="header_hr" />
+			</div>
+    );
+  }
+})
+
+var Bio = React.createClass({
+  render: function () {
+    return (
+      <div className='col-md-8 col-md-offset-2'>
+				<h1>Bio</h1>
+				<p>My name is Rostyslav. Im web developer. I live and work in Ukraine. <br/> <br/>
+					<b>Skills:</b>
+					<ul>
+						<li>Ruby</li>
+					 	<li>Ruby on Rails</li>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>JavaScript</li>
+						<li>jQuery</li>
+						<li>Ajax</li>
+						<li>ReactJS</li>
+					</ul>
+				</p>
+      </div>
+    );
+  }
+})
+
+ReactDOM.render(<App />, document.getElementById('app'))
