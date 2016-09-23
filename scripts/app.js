d190a2b41a322d89645a8bb47475b2134034f591
TeTiRoss <rostyk.s@hmarynka.com>
2016-09-23 14:30:51 +0300

    working react app

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,18 @@ var App = React.createClass({
   render: function () {
     return (
       <div>
-        <Header />
-        <Bio />
+        <ReactHeader />
+        <ListItemContainer />
+      </div>
+    );
+  }
+});
+
+var ReactHeader = React.createClass({
+  render: function () {
+    return (
+      <div className='col-md-8 col-md-offset-2'>
+        <h1>React apps</h1>
       </div>
     );
   }
@@ -28,7 +38,7 @@ var Bio = React.createClass({
     return (
       <div className='col-md-8 col-md-offset-2'>
 				<h1>Bio</h1>
-				<p>My name is Rostyslav. Im web developer. I live and work in Ukraine. <br/> <br/>
+				<p>My name is Rostyslav. I&#39;m web developer. I live and work in Ukraine. <br/> <br/>
 					<b>Skills:</b>
 					<ul>
 						<li>Ruby</li>
@@ -45,5 +55,87 @@ var Bio = React.createClass({
     );
   }
 })
+
+var ListItemContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      list_items: ['example']
+    }
+  },
+
+  handleSubmit: function(text) {
+    var list_items = this.state.list_items;
+    var new_list = list_items.concat([text]);
+    this.setState({
+      list_items: new_list,
+    });
+  },
+
+  render: function() {
+    var list_items = this.state.list_items.map(function(item) {
+      return <ListItem text={item} />
+    });
+
+    return (
+      <div className='col-md-8 col-md-offset-2'>
+        <div className='react_item_container'>
+          <p>Simple form built on ReactJS that allows you to add items to the list.</p>
+
+          <ListItemForm onFormSubmit={this.handleSubmit} />
+          <br/>
+
+          <ul>
+            {list_items}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+var ListItem = React.createClass({
+  render: function() {
+    return (
+      <li>{this.props.text}</li>
+    );
+  }
+});
+
+var ListItemForm = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
+
+  handleTextChange: function(e) {
+    var new_value = e.target.value;
+    this.setState({text: new_value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var text = this.state.text;
+
+    this.setState({text: ''});
+    this.props.onFormSubmit(text);
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}
+       className='form-inline'>
+        <div className="input-group">
+          <input type='text' value={this.state.text}
+           onChange={this.handleTextChange} className='form-control input-sm'/>
+          <span className="input-group-btn">
+            <button className='btn btn-sm btn-info'>Add to list</button>
+          </span>
+        </div>
+      </form>
+    );
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('app'))
